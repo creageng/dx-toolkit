26680b0017a8c6d35e4cac28ed160518fed38ab1
Aleksandra Zalcman <azalcman@dnanexus.com>
2017-11-09 09:56:35 -0800

    Print exception

--- src/python/test/test_dxclient.py
+++ src/python/test/test_dxclient.py
@@ -2555,10 +2555,8 @@ dx-jobutil-add-output record_array $second_record --array
         # --watch implies --priority high
         try:
             run("dx run myapplet -y --watch")
-        except subprocess.CalledProcessError:
-            # ignore any watching errors; just want to test requested
-            # priority
-            pass
+        except subprocess.CalledProcessError as e:
+            print(e)
         watched_job_id = run("dx find jobs -n 1 --brief").strip()
         self.assertNotIn(watched_job_id, [normal_job_id, high_priority_job_id])
         watched_job_desc = dxpy.describe(watched_job_id)
